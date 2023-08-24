be2e1f4e97286cec9a411afb3549a7908dbcaf08
Joe Junior Nyoni <juniornyoni@gmail.com>
2023-08-24 11:03:20 +0200

    Employee detail list initial implementation

--- src/app/components/ui/employee-detail/employee-detail.component.ts
+++ src/app/components/ui/employee-detail/employee-detail.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Select } from '@ngxs/store';
+
+import { EmployeeState } from 'src/app/store/employee/employee.state';
+import { Employee } from 'src/app/models/employee';
 
 @Component({
   selector: 'app-employee-detail',
   templateUrl: './employee-detail.component.html',
   styleUrls: ['./employee-detail.component.sass']
 })
-export class EmployeeDetailComponent {
+export class EmployeeDetailComponent implements OnInit, OnDestroy {
+  @Select(EmployeeState.getEmployees) employees$!: Observable<any[]>;
+  employees: Employee[] = [];
+  private subscription!: Subscription;
+  
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.subscription = this.employees$.subscribe((data) => {
+      this.employees = data[0];
+      console.log('huh', this.employees);
+    })
+  }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  trackByFn(index: number) {
+    return index;
+  }
 }
