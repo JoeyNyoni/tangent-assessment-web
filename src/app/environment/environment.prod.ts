a9ebae10f7db59438830c5cc7304af124c259d8f
Joe Junior Nyoni <juniornyoni@gmail.com>
2023-08-24 23:59:49 +0200

    Re-add HTTP endpoints

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment  = {
   production: true,
-  API_URL: "http://localhost:5255/api",
+  API_URL: "https://localhost:7288/api",
 }
--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment  = {
   production: false,
-  API_URL: "http://localhost:5255/api",
+  API_URL: "https://localhost:7288/api",
 }
